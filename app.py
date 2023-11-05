752bf29361974eafeb0f73b4a71dcc10b72847e2
zihhhh <127283130+zihhhh@users.noreply.github.com>
2023-11-05 19:09:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ import datetime as dt
 from datetime import datetime
 import googletrans
 import csv
-
+import time, statistics
 import BubbleMsg
 import ImagemapMsg
 import CarouselMsg
@@ -525,7 +525,7 @@ def handle_text_message(event):
             date=date.text
             list_date.append(date)
 
-        for x in range(0,4):     
+        for x in range(0,3):     
             bubble = BubbleContainer(
                                 direction='ltr', 
                                 body=BoxComponent(  
@@ -857,7 +857,7 @@ def handle_text_message(event):
                 status = 0
         else:
             pass
-
+'''
 @handler.add(MessageEvent, message=LocationMessage)
 def handle_location_message(event):
     line_bot_api.reply_message(
@@ -867,16 +867,50 @@ def handle_location_message(event):
             latitude=event.message.latitude, longitude=event.message.longitude
         )
     )
+'''
 
-
-@handler.add(MessageEvent, message=StickerMessage)
-def handle_sticker_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        StickerSendMessage(
-            package_id=event.message.package_id,
-            sticker_id=event.message.sticker_id)
-    )
+# 空氣品質函式
+@handler.add(MessageEvent, message=LocationMessage)
+def aqi(event):
+    if isinstance(event.message, LocationMessage):
+        global status
+        address=event.message.address.replace('台','臺')
+        print(address)
+        city_list, site_list ={}, {}
+        
+        try:
+            # 2022/12 時氣象局有修改了 API 內容，將部份大小寫混合全改成小寫，因此程式碼也跟著修正
+            url = 'https://data.epa.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON'
+            a_data = requests.get(url)             # 使用 get 方法透過空氣品質指標 API 取得內容
+            a_data_json = a_data.json()            # json 格式化訊息內容
+            for i in a_data_json['records']:       # 依序取出 records 內容的每個項目
+                city = i['county']                 # 取出縣市名稱
+                if city not in city_list:
+                    city_list[city]=[]             # 以縣市名稱為 key，準備存入串列資料
+                site = i['sitename']               # 取出鄉鎮區域名稱
+                aqi = int(i['aqi'])                # 取得 AQI 數值
+                status = i['status']               # 取得空氣品質狀態
+                site_list[site] = {'aqi':aqi, 'status':status}  # 記錄鄉鎮區域空氣品質
+                city_list[city].append(aqi)        # 將各個縣市裡的鄉鎮區域空氣 aqi 數值，以串列方式放入縣市名稱的變數裡
+            for i in city_list:
+                if i in address: # 如果地址裡包含縣市名稱的 key，就直接使用對應的內容
+                    # 參考 https://airtw.epa.gov.tw/cht/Information/Standard/AirQualityIndicator.aspx
+                    aqi_val = round(statistics.mean(city_list[i]),0)  # 計算平均數值，如果找不到鄉鎮區域，就使用縣市的平均值
+                    aqi_status = ''  # 手動判斷對應的空氣品質說明文字
+                    if aqi_val<=50: aqi_status = '良好'
+                    elif aqi_val>50 and aqi_val<=100: aqi_status = '普通'
+                    elif aqi_val>100 and aqi_val<=150: aqi_status = '對敏感族群不健康'
+                    elif aqi_val>150 and aqi_val<=200: aqi_status = '對所有族群不健康'
+                    elif aqi_val>200 and aqi_val<=300: aqi_status = '非常不健康'
+                    else: aqi_status = '危害'
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text='空氣品質'+aqi_status+'AQI'+ aqi_val))
+                    break
+            for i in site_list:
+                if i in address:  # 如果地址裡包含鄉鎮區域名稱的 key，就直接使用對應的內容
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text='空氣品質'+site_list[i]["status"]+'AQI'+ site_list[i]["aqi"]))
+                    break
+        except:
+             line_bot_api.reply_message(event.reply_token, TextSendMessage(text='找不到空氣品質資訊。'))
 
 
 # Other Message Type
@@ -1657,6 +1691,8 @@ def check_filter(userData_dict, attr):
             return True
         else:
             return False
+
+
 
 
 if __name__ == "__main__":
