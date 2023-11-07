39e7e08a8e210f7ab834377fb4a55503a790917c
zihhhh <127283130+zihhhh@users.noreply.github.com>
2023-11-07 23:58:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -330,20 +330,6 @@ def handle_text_message(event):
                 TextSendMessage(text='請輸入您欲食用的食物名稱')
             ])    
             status = 21
-    elif text =="運動計畫規劃":
-            line_bot_api.reply_message(event.reply_token, [
-                TextSendMessage(text='請輸入您的目標或想從事的運動種類，如：一周消耗3000卡、跑步')
-            ])    
-            status = 22        
-    elif text=="空氣品質查詢":
-        message = TextSendMessage(
-                text='點選定位並分享位置訊息以獲取空氣品質資訊',
-                quick_reply=QuickReply(
-                    items=[
-                        QuickReplyButton(action=LocationAction(label="定位"))
-                        ]
-            ))
-        line_bot_api.reply_message(event.reply_token,message)        
     
 
     elif text == '查詢紀錄': #查訊紀錄
@@ -629,7 +615,6 @@ def handle_text_message(event):
                 line_bot_api.reply_message(event.reply_token, TextSendMessage(text='格式錯誤，請重新輸入'))
         #新功能
         elif status == 20:    
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text="努力運轉中，請稍後"))
             print("使用chat gpt")
             messages = [
                 #賦予人設
@@ -661,23 +646,6 @@ def handle_text_message(event):
             messages=messages)
             content = response['choices'][0]['message']['content']
             line_bot_api.reply_message(event.reply_token,TextSendMessage(text=content.strip()))
-
-        elif status == 22:    
-            print("使用chat gpt")
-            messages = [
-                #賦予人設
-                {'role': 'system', 'content': '請設計一份300字以內的運動計畫且依據以下條件：'}, 
-    
-                #提出問題
-                {'role': 'user','content': event.message.text}
-                ]
-            response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            #max_tokens=128,
-            temperature=0.5,
-            messages=messages)
-            content = response['choices'][0]['message']['content']
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text=content.strip()))    
         
         elif status == 3: # water intake
             if not isNum(text):
@@ -909,42 +877,39 @@ def aqi(event):
         address=event.message.address.replace('台','臺')
         print(address)
         city_list, site_list ={}, {}
-        msg='找不到空氣品質資訊。'
-        try:
-            # 2022/12 時氣象局有修改了 API 內容，將部份大小寫混合全改成小寫，因此程式碼也跟著修正
-            url = 'https://data.epa.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON'
-            a_data = requests.get(url)             # 使用 get 方法透過空氣品質指標 API 取得內容
-            a_data_json = a_data.json()            # json 格式化訊息內容
-            for i in a_data_json['records']:       # 依序取出 records 內容的每個項目
-                city = i['county']                 # 取出縣市名稱
-                if city not in city_list:
-                    city_list[city]=[]             # 以縣市名稱為 key，準備存入串列資料
-                site = i['sitename']               # 取出鄉鎮區域名稱
-                aqi = int(i['aqi'])                # 取得 AQI 數值
-                status = i['status']               # 取得空氣品質狀態
-                site_list[site] = {'aqi':aqi, 'status':status}  # 記錄鄉鎮區域空氣品質
-                city_list[city].append(aqi)        # 將各個縣市裡的鄉鎮區域空氣 aqi 數值，以串列方式放入縣市名稱的變數裡
-            for i in city_list:
-                if i in address: # 如果地址裡包含縣市名稱的 key，就直接使用對應的內容
-                    # 參考 https://airtw.epa.gov.tw/cht/Information/Standard/AirQualityIndicator.aspx
-                    aqi_val = round(statistics.mean(city_list[i]),0)  # 計算平均數值，如果找不到鄉鎮區域，就使用縣市的平均值
-                    aqi_status = ''  # 手動判斷對應的空氣品質說明文字
-                    if aqi_val<=50: aqi_status = '良好'
-                    elif aqi_val>50 and aqi_val<=100: aqi_status = '普通'
-                    elif aqi_val>100 and aqi_val<=150: aqi_status = '對敏感族群不健康'
-                    elif aqi_val>150 and aqi_val<=200: aqi_status = '對所有族群不健康'
-                    elif aqi_val>200 and aqi_val<=300: aqi_status = '非常不健康'
-                    else: aqi_status = '危害'
-                    msg='空氣品質：'+str(aqi_status) +'  AQI：'+str(aqi_val)
-                    break
-            for i in site_list:
-                if i in address:  # 如果地址裡包含鄉鎮區域名稱的 key，就直接使用對應的內容
-                    msg=='空氣品質：'+str(site_list[i]["status"])+'  AQI：'+ str(site_list[i]["aqi"])
-                    break
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))    
-        except:
-             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
-
+        msg = '找不到空氣品質資訊。'
+        
+        # 2022/12 時氣象局有修改了 API 內容，將部份大小寫混合全改成小寫，因此程式碼也跟著修正
+        url = 'https://data.epa.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON'
+        a_data = requests.get(url)             # 使用 get 方法透過空氣品質指標 API 取得內容
+        a_data_json = a_data.json()            # json 格式化訊息內容
+        for i in a_data_json['records']:       # 依序取出 records 內容的每個項目
+            city = i['county']                 # 取出縣市名稱
+            if city not in city_list:
+                city_list[city]=[]             # 以縣市名稱為 key，準備存入串列資料
+            site = i['sitename']               # 取出鄉鎮區域名稱
+            aqi = int(i['aqi'])                # 取得 AQI 數值
+            status = i['status']               # 取得空氣品質狀態
+            site_list[site] = {'aqi':aqi, 'status':status}  # 記錄鄉鎮區域空氣品質
+            city_list[city].append(aqi)        # 將各個縣市裡的鄉鎮區域空氣 aqi 數值，以串列方式放入縣市名稱的變數裡
+        for i in city_list:
+            if i in address: # 如果地址裡包含縣市名稱的 key，就直接使用對應的內容
+                # 參考 https://airtw.epa.gov.tw/cht/Information/Standard/AirQualityIndicator.aspx
+                aqi_val = round(statistics.mean(city_list[i]),0)  # 計算平均數值，如果找不到鄉鎮區域，就使用縣市的平均值
+                aqi_status = ''  # 手動判斷對應的空氣品質說明文字
+                if aqi_val<=50: aqi_status = '良好'
+                elif aqi_val>50 and aqi_val<=100: aqi_status = '普通'
+                elif aqi_val>100 and aqi_val<=150: aqi_status = '對敏感族群不健康'
+                elif aqi_val>150 and aqi_val<=200: aqi_status = '對所有族群不健康'
+                elif aqi_val>200 and aqi_val<=300: aqi_status = '非常不健康'
+                else: aqi_status = '危害'
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text='空氣品質'+aqi_status+'AQI'+ aqi_val))
+                break
+        for i in site_list:
+            if i in address:  # 如果地址裡包含鄉鎮區域名稱的 key，就直接使用對應的內容
+                msg = f'空氣品質{site_list[i]["status"]} ( AQI {site_list[i]["aqi"]} )。'
+                break
+        
 
 # Other Message Type
 @handler.add(MessageEvent, message=(ImageMessage, VideoMessage, AudioMessage))
